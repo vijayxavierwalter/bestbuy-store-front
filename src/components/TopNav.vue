3ce12268430e1d595419fde9a83c29d466ab32f5
vijayxavierwalter <walt0180@algonquinlive.com>
2026-04-22 10:44:08 -0400

    Rebrand store-front navigation to Best Buy UI

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -1,13 +1,16 @@
+```vue
 <template>
   <nav>
     <div class="logo">
-      <a href="/">
-        <img src="/algonquin-pet-store.png" alt="Algonquin Pet Store Logo">
-      </a>
+      <router-link to="/">
+        <span class="brand">Best Buy Store</span>
+      </router-link>
     </div>
+
     <button class="hamburger" @click="toggleNav">
       <span class="hamburger-icon"></span>
     </button>
+
     <ul class="nav-links" :class="{ 'nav-links--open': isNavOpen }">
       <li><router-link to="/" @click="closeNav">Products</router-link></li>
       <li><router-link to="/cart" @click="closeNav">Cart ({{ cartItemCount }})</router-link></li>
@@ -40,28 +43,40 @@ nav {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  background-color: #3c673cd7;
+  background-color: #0046be; /* Best Buy blue */
   color: #fff;
-  padding-top: 0.5rem;
-  padding-left: 1rem;
-  padding-right: 1rem;
-  padding-bottom: 0.25rem;
+  padding: 0.75rem 1rem;
   position: fixed;
   top: 0;
   left: 0;
   right: 0;
 }
 
-nav img {
-  width: 60px;
-  height: auto;
+.brand {
+  font-size: 1.5rem;
+  font-weight: bold;
+  color: #fff;
+  text-decoration: none;
 }
 
 .nav-links {
   display: flex;
   list-style: none;
-  font-size: 1.5rem;
+  font-size: 1.2rem;
   font-weight: bold;
+}
+
+.nav-links li {
+  margin-left: 1.5rem;
+}
+
+.nav-links a {
+  color: #fff;
+  text-decoration: none;
+}
+
+.nav-links a:hover {
+  text-decoration: underline;
 }
 
 .hamburger {
@@ -69,9 +84,6 @@ nav img {
   background: none;
   border: none;
   cursor: pointer;
-  padding: 0;
-  margin: 0;
-  margin-top: -40px;
 }
 
 .hamburger-icon {
@@ -80,8 +92,6 @@ nav img {
   height: 2px;
   background-color: #fff;
   position: relative;
-  top: 50%;
-  transform: translateY(-50%);
 }
 
 .hamburger-icon::before,
@@ -110,7 +120,7 @@ nav img {
     top: 100%;
     left: 0;
     right: 0;
-    background-color: #333;
+    background-color: #0046be;
     padding: 1rem;
   }
 
@@ -127,3 +137,4 @@ nav img {
   }
 }
 </style>
+```
